c47b9492c931dfeec38386640bc13ccafb8f5d61
Aleh Kasiankou <aleh.kasiankou@gmail.com>
2023-01-26 11:38:38 +0300

    add docker-compose

--- Innowise.Clinic.Auth/Program.cs
+++ Innowise.Clinic.Auth/Program.cs
@@ -12,12 +12,9 @@ builder.Services.RegisterDbContext(builder.Configuration.GetConnectionString("de
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
