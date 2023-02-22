89ae2e16ea222cc1a032ad22d6f501528ad79ce7
Aleh Kasiankou <oleg.kosenkov@innowise-group.com>
2023-02-22 13:48:50 +0300

    validate emails via post requests

--- Innowise.Clinic.Auth.Api/Controllers/ValidationController.cs
+++ Innowise.Clinic.Auth.Api/Controllers/ValidationController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Innowise.Clinic.Auth.Api.Controllers.Abstractions;
 using Innowise.Clinic.Auth.Validators.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -28,10 +29,10 @@ public class ValidationController : ApiControllerBase
     /// </returns>
     /// <response code="200">Success. The email is not registered in the system.</response>
     /// <response code="400"> Fail. The account with the provided email is already registered in the system.</response>
-    [HttpGet("email/{email:required}")]
+    [HttpPost("email")]
     [ProducesResponseType(typeof(void), 200)]
     [ProducesResponseType(typeof(void), 400)]
-    public async Task<IActionResult> CheckEmailUniqueness([FromRoute] string email)
+    public async Task<IActionResult> CheckEmailUniqueness([FromBody] [EmailAddress] string email)
     {
         var isValidationSucceeded = await _emailValidator.ValidateEmailAsync(email);
 
